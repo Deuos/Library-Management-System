b86f324e397ec9298d24188f3b6fcff3d18a6323
Construct-Chris <116314422+Construct-Chris@users.noreply.github.com>
2023-05-03 18:47:47 -0400

    Add attribution

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,17 @@
+/*
+    Library Management System
+    Group 3
+	Owen Bessette, Chris Gerk,
+	Jin Lee, Kush Patel, and Sereyvichea Phan
+    Professor Gao
+	CSC-445-01 Software Engineering
+	
+	Code sourced and modified from
+	MDN Express web framework (Node.js/JavaScript)
+	learning resources for beginning developers and students
+	https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs
+*/
+
 // Require user model
 const User = require('../models/user');
 
